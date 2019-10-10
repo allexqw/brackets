727161a85042ecf18abf69ac500f364cd1ea7c4b
Alexandr Hotyan <alexhotyan@gmail.com>
2019-10-10 18:29:11 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let a =0;
+let b = 0;
+for (let i = 0; i < bracketsConfig.length; i++) {
+  for (let j = 0; j < str.length; j++) {
+    if(bracketsConfig[i][0]==bracketsConfig[i][1]){
+        continue   
+    }
+    if (bracketsConfig[i][0] == str[j]) {
+        a++;
+      } else if (bracketsConfig[i][1] == str[j]) {
+        if(a>b){
+          b++;
+        }
+      }
+    }
+}
+if (a == b) return true;
+else return false;
 }
